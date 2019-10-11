b224e573f9d0b8d0676a8a2cf56600f814b733d8
Henri BERGER <hberger@e1r4p4.42.fr>
2019-10-11 19:26:39 +0200

    Updt Substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: hberger <hberger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 16:56:21 by hberger           #+#    #+#             */
-/*   Updated: 2019/10/10 15:32:46 by hberger          ###   ########.fr       */
+/*   Updated: 2019/10/11 19:15:33 by hberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,13 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*output;
 
+	if (s && start > ft_strlen(s))
+	{
+		if (!(output = (char*)malloc(1)))
+			return (0);
+		output[0] = '\0';
+		return (output);
+	}
 	if (s == 0 || (output = (char*)malloc(sizeof(char) * (len + 1))) == 0)
 		return (0);
 	output = ft_memcpy(output, s + start, len);
