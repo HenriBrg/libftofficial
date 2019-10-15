9bc070e04b25f733ad37b4515c9358d033e12a1e
Henri BERGER <hberger@e1r13p10.42.fr>
2019-10-15 20:05:56 +0200

    fix addfront

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: hberger <hberger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 17:33:27 by hberger           #+#    #+#             */
-/*   Updated: 2019/10/15 17:43:59 by hberger          ###   ########.fr       */
+/*   Updated: 2019/10/15 19:29:53 by hberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@
 
 void	ft_lstadd_front(t_list **alst, t_list *new)
 {
-	if (*alst == 0 || alst == 0 || new == 0)
-		return ;
 	new->next = *alst;
 	*alst = new;
 }
